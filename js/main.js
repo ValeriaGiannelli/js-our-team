91f1d54f8badee6884a60a5e89ed0c1ad62b02e2
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-18 14:41:52 +0200

    feat: create array of obj

--- js/main.js
+++ js/main.js
@@ -8,5 +8,37 @@ MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe*/
 
 
-
+// array di oggetti con i memebri del team:
+const ourTeam = [
+    {
+        "nome": "Wayne Barnett",
+        "ruolo": "Founder & CEO",
+        "foto": "img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "nome": "Angela Caroli",
+        "ruolo": "Chief Editor",
+        "foto": "img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        "nome": "Walter Gordon",
+        "ruolo": "Office Manager",
+        "foto": "img/walter-gordon-office-manager.jpg"
+    },
+    {
+        "nome": "Angela Lopez",
+        "ruolo": "Social Media Manager",
+        "foto": "img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "nome": "Scott Estrada",
+        "ruolo": "Developer",
+        "foto": "img/scott-estrada-developer.jpg"
+    },
+    {
+        "nome": "barbara Ramos",
+        "ruolo": "Graphic Designer",
+        "foto": "img/barbara-ramos-graphic-designer.jpg"
+    },
+];
 
